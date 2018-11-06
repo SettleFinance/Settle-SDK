963c17ad5f111194e331cb4828587df616b4dcdd
roni <roni.davelman@gmail.com>
2018-11-06 18:01:48 -0500

    publish api

---
+++ Examples/exchange-token.js
@@ -0,0 +1,20 @@
+const Settle = require('../settle')
+
+/**
+
+Generate a public and private api key at https://settle.finance/os
+Make sure your .env file reflects your keys to make authenticated requests.
+
+**/
+
+
+/** Exchange Token **/
+
+// The token is passed as a get param (?user_id) into your app, along with the app mode (?mode)
+var token = '';
+
+// Convert a users token to a GUID to make requests for their data
+Settle.exchangeTokenForGuid(token)
+.then((result) => {
+  console.log(result)
+})
