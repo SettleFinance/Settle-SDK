783fcf535120f7b6f532010fd064b1f4b8713588
Adam Peretti <adam.peretti@gmail.com>
2018-11-14 14:26:49 -0800

    Stop Tracking .env

--- Examples/portfolio.js
+++ Examples/portfolio.js
@@ -25,33 +25,38 @@ Settle.Portfolio.Summary({guid})
 .then((result) => {
   console.log('summary')
 })
-
+.catch(e => console.log(e))
 // Return a users holdings and total portfolio value
 Settle.Portfolio.Holdings({guid})
 .then((result) => {
   console.log('holdings')
 })
+.catch(e => console.log(e))
 
 // Return a users holdings with a list of trades for each asset
 Settle.Portfolio.HoldingsWithTrades({guid})
 .then((result) => {
   console.log('holdings with trades')
 })
+.catch(e => console.log(e))
 
 // Return a users trades
 Settle.Portfolio.Trades({guid})
 .then((result) => {
   console.log('trades')
 })
+.catch(e => console.log(e))
 
 // Return a users exchanges
 Settle.Portfolio.Exchanges({guid})
 .then((result) => {
   console.log('exchanges')
 })
+.catch(e => console.log(e))
 
 // Return a users 30 day balance history (for graphing on a chart)
 Settle.Portfolio.BalanceHistory({guid})
 .then((result) => {
   console.log('balance history')
 })
+.catch(e => console.log(e))
