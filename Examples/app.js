5f5f66fc36e1f2704cac061ff8ab38512fa81f87
Adam Peretti <adam.peretti@gmail.com>
2018-11-14 13:45:33 -0800

    Add Send Notificaiton Example

--- Examples/app.js
+++ Examples/app.js
@@ -10,8 +10,17 @@ Make sure your .env file reflects your keys to make authenticated requests.
 
 /** App **/
 
+let quid = "785035bc-2b83-478f-9fa5-371b64b66e67"
+
+
 // Return a list of users who installed your app
 Settle.App.Users()
 .then((result) => {
   console.log(result)
 })
+
+Settle.App.SendNotification({guid: guid})
+.then(res => console.log(res))
+.catch(e => console.log(e))
+
+
