f034831d7d9656fa90fbe3f4e046217d42b09d59
Ben Harloe <ben.harloe@gmail.com>
2018-11-16 15:23:10 -0800

    clean up

--- Examples/user.js
+++ Examples/user.js
@@ -8,11 +8,9 @@ Make sure your .env file reflects your keys to make authenticated requests.
 **/
 
 
-/** App **/
+/** User **/
 
 var guid = {guid: "881e3777-a9d6-4e8c-a85f-b0d8d6fb0bde"}
-
-// console.log(process.env.SETTLE_API_KEY)
 
 Settle.User.GuidToUsername(guid)
 .then((result) => {
