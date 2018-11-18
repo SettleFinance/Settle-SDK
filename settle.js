993738b9a6b1a5df31f725b4ccfe97071d09554a
arman37 <arman.it37@gmail.com>
2018-11-19 01:47:41 +0600

    Wrap chat module

--- settle.js
+++ settle.js
@@ -3,9 +3,10 @@ const axios = require('axios')
 const crypto = require('crypto')
 const PriceFeed = require('./SDK/price-feed')
 const Portfolio = require('./SDK/portfolio')
-const App = require('./SDK/app')
+const App = require('./SDK/app');
+const Chat = require('./SDK/chat');
 
-const AuthSecurity = require('./Security/AuthSecurity')
+const AuthSecurity = require('./Security/AuthSecurity');
 
 var Settle = {
   exchangeTokenForGuid: function ExchangeTokenForGuid(token) {
@@ -13,7 +14,8 @@ var Settle = {
   },
   PriceFeed: PriceFeed,
   Portfolio: Portfolio,
-  App: App
-}
+  App: App,
+  Chat
+};
 
 module.exports = Settle;
